211fe0ff00b9159332ad016b192adbdd88775247
Alonsovpm <avpizarro@miuandes.cl>
2021-06-14 16:14:59 -0400

    inputform/listform

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const {getTitle} = require('./view')
+const {inputForm, listForm} = require('./view')
+const {printTable} = require('console-table-printer')
 
-function app(state,update,view){
+async function app(state,update,view){
+    const {model, currentView} = state
+    const {title, table} = currentView
     console.clear
-    console.log(getTitle())
+    console.log(title)
+    console.log("NO CITIES")
+    const selection = await listForm(model)
+    const location = await inputForm(model)
+    printTable(table)
+
 }
 
 module.exports = {
--- view.js
+++ view.js
@@ -1,5 +1,6 @@
 const figlet = require('figlet')
 const chalk = require('chalk')
+const inquirer = require('inquirer')
 
 function getTitle(){
     return chalk.green(
@@ -12,6 +13,58 @@ function getTitle(){
     )
 }
 
+function getTable(model){
+    const {name} = model
+    const {temp} = model
+    const {max} = model
+    const {min} = model
+    return[
+        {
+            'name': name,
+            'temp': temp,
+            'max': max,
+            'min': min,
+        }
+    ]
+}
+
+function inputForm(model){
+    const{input} = model
+    const message = 'Location? '
+    return inquirer.prompt([
+        {
+            name: 'location',
+            type: 'input',
+            message: message,
+            default: input,
+        }
+    ])
+}
+function listForm(model){
+    const {input} = model
+    const message = 'Select action: '
+    const choices1 = ['Add City']
+    const choices2 = ['Add City','Update City','Delete City']
+    return inquirer.prompt([
+        {
+            name: 'action',
+            type: 'list',
+            message: message,
+            default: input,
+            choices: choices1
+        }
+    ])
+}
+
+function view(model){
+    return {
+        title: getTitle(),
+        table: getTable(model)
+    }
+}
+
 module.exports = {
-    getTitle
+    view,
+    inputForm,
+    listForm
 } 
